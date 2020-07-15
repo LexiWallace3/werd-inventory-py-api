97a7336efb6e800ff000d1c21ef7c7b50fb6c7da
Sydnee Gage <sgage2@students.kennesaw.edu>
2020-07-15 11:04:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -95,95 +95,51 @@ SUR = Accessories(title='SUR', colorsused='gold/silver', type='Necklace', quanti
 
 SUR.save()
 
-
-LoveHurtsSweats = Clothes(title='Love Hurts', size='m',
-                          colorsused='black/white/yellow/green', type='sweat pant', quantity=1)
-LoveHurtsSweats.save()
-
-LoveHurtsCrewneck = Clothes(title='Love Hurts', size='m',
-                            colorsused='black/white/red', type='crew neck', quantity=1)
-LoveHurtsCrewneck.save()
-
-LogoShorts = Clothes(title='Logo Shorts', size='m',
-                     colorsused='white/black', type='shorts', quantity=1)
-LogoShorts.save()
-
-InfiniteThought = Clothes(title='Infinite Thought', size='m',
-                          colorsused='tan/black/blue', type='hoodie', quantity=1)
-InfiniteThought.save()
-
-ReaperSweats = Clothes(title='Reaper', size='m',
-                       colorsused='gray/black/red', type='sweat pant', quantity=1)
-ReaperSweats.save()
-
-CartiJeanJacket = Clothes(title='Playboi Carti Jean Jacket', size='l',
-                          colorsused='black/green/red/white', type='jean jacket', quantity=1)
-CartiJeanJacket.save()
-
-werdXchampionFlowers = Clothes(title='Werd x Champion', size='m',
-                               colorsused='navy blue/white/green/yellow', type='hoodie', quantity=1)
-werdXchampionFlowers.save()
-
-Sakura = Clothes(title='Sakura', size='m, l, xl',
-                 colorsused='white/red/pink/blue', type='hoodie', quantity=4)
-Sakura.save()
+RoriLeatherBelt = Accessories(title='Rori Leather Belt,
+                     colorsused='black/brown/cream/sage', type='belt', quantity=20)
+                     
+RoriLeatherBelt.save()
 
 
-NeonLogo = Clothes(title='Neon Logo', size='xl',
-                   colorsused='neon green/red', type='tShirt', quantity=1)
-NeonLogo.save()
+GigLeatherChain = Accessories(title='Gigi Leather Chain',
+                     colorsused='Black', type='belt', quantity=6)
 
-JapanesesWerdShort = Clothes(
-    title="'Werd' Shorts", size='m', colorsused='white', type='shorts', quantity=1)
-JapanesesWerdShort.save()
-
-OxymoronHoodie = Clothes(title='Oxy-moron', size='l',
-                         colorsused='blue/black/yellow/green', type='hoodie', quantity=1)
-OxymoronHoodie.save()
-
-WhatchuWant = Clothes(title='Whatchu want from me', size='m, l',
-                      colorsused='white/black/green/blue', type='tShirt', quantity=3)
-WhatchuWant.save()
+GigiLeatherChain.save()
 
 
-WhatchuWantHoodie = Clothes(title='Whatchu want from me', size='m',
-                            colorsused='white/black/green/blue', type='hoodie', quantity=2)
-WhatchuWantHoodie.save()
+ Suede= Accessories(title='Suede',
+                          colorsused='tan/black/brown', type='belt', quantity=12)
+Suede.save()
 
 
-WhyTshirt = Clothes(title='Why', size='m, l, xl',
-                    colorsused='white/brown/black/green', type='tShirt', quantity=4)
-WhyTshirt.save()
-
-FlipSide = Clothes(title='Flip Side', size='m, l, xl',
-                   colorsused='white/black/blue/', type='tShirt', quantity=5)
-FlipSide.save()
-
-LongSleeveLogo = Clothes(title='Long Sleeve Logo', size='m, l, xl',
-                         colorsused='white/red', type='long sleeve', quantity=3)
-LongSleeveLogo.save()
+Fedora = Acessories(title='Fedora',
+                     colorsused='cream/black/olive/dusty', type='hats', quantity=20)
+Fedora.save()
 
 
-Spider = Clothes(title='Spider', size='m',
-                 colorsused='black/white/orange', type='tShirt', quantity=1)
-Spider.save()
+ SuperScrunchie = Accessories(title='Super Scrunchie',
+                          colorsused='tan/black/sage/orange/dusty', type='Hair', quantity=12)
+SuperScrunchie.save()
 
 
-NavyBlueLogo = Clothes(title='Navy Blue Logo', size='m',
-                       colorsused='navy blue/white/red', type='tShirt', quantity=1)
-NavyBlueLogo.save()
+ DaydreamScarfPony= Accessories(title='Daydream Scarf Pony',
+                          colorsused='gold/olive/blue floral/midnight garden', type='Hair', quantity=12)
+DaydreamScarfPony.save()
 
-NoHardFeelings = Clothes(title='No Hard Feelings', size='m',
-                         colorsused='navy blue/yellow/red', type='tShirt', quantity=1)
-NoHardFeelings.save()
+ TopKnotHeadband= Accessories(title='Top Knot Headband',
+                          colorsused='gold/black', type='Hair', quantity=12)
+TopKnotHeadband.save()
 
-AllEveryEverWanted = Clothes(title='All Everybody Ever Wanted', size='m',
-                             colorsused='tan/navy blue/white', type='tShirt', quantity=1)
-AllEveryEverWanted.save()
+ PrettyMessBarrette= Accessories(title='Pretty Mess Barrette',
+                          colorsused='gold', type='Hair', quantity=12)
+PrettyMessBarrette.save() 
 
-SeeYouNever = Clothes(title='See You Never', size='m, l',
-                      colorsused='black/red/white', type='hoodie', quantity=3)
-SeeYouNever.save()
+ BeadedClips= Accessories(title='Beaded Clips',
+                          colorsused='Blush/Mint/Peach/Bronze/Ivory', type='Hair', quantity=12)
+BeadedClips.save() 
+
+
+
 
 
 print('')
